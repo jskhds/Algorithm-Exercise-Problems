8ed316491be0e38215cd88e2f63fcd38043bdb0a
jskhds <2987163443a@gmail.com>
2021-10-24 19:39:33 +0800

    回文链表判断

---
+++ 08-链表/206-反转链表.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    let currNode =  head;
+    let prev = null;
+    while(currNode){
+        const next = currNode.next;
+        currNode.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    
+    return prev;
+
+    
+
+     
+     
+};
+// 递归法
+var reverseList1 = function(head) {
+     if(head == nul || head.next == null){
+        return head;
+     }
+     const p = reverseList1(head.next);
+     head.next.next = head;
+     head.next = null;
+     return p;
+
+    
+
+     
+     
+};
+
+ 
