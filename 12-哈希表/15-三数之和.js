d475f06dcdaaaa4787fe019291449e4c2212787f
jskhds <2987163443a@gmail.com>
2021-11-15 23:15:54 +0800

    补充三数四数之和代码

--- 12-哈希表/15-三数之和.js
+++ 12-哈希表/15-三数之和.js
@@ -2,4 +2,39 @@
 // 请你找出所有和为 0 且不重复的三元组。
 // 注意：答案中不可以包含重复的三元组。
 
- 
+//  用哈希存两个数的和 然后再找第三个数 但是去重比较复杂 所以最好还是用双指针
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+    //  从小到大排序
+     nums.sort((a,b)=>a-b);
+     let res = [];
+    // 从第 i个开始 left从i+1开始 right从末尾开始 相加大于0 right移动 小于0 left移动
+     for(let i = 0;i<nums.length;i++){
+         if (nums[i]>0) break; //因为数组已经从小到大排序了
+         let left = i+1; //nums[i]已经确定了索引 左指针从i后一个开始
+         let right = nums.length - 1;
+        //  这步用来去重
+         if(i > 0 && nums[i] == nums[i-1]) continue;        
+         while(left<right)
+         {
+            let sum = nums[i] + nums[left] + nums[right];
+            if(sum == 0){
+                res.push([nums[i],nums[left],nums[right]]);
+                while (left<right && nums[left] == nums[left+1]) left++; // 因为左边要往右移动  所以要加找到不重复的为止
+                while (left<right && nums[right] == nums[right-1]) right--; // 因为右边要往左移动 同上
+                left++;
+                right--;
+            } 
+            else if(sum>0){
+               right--; 
+            }
+            else{
+               left++; 
+            }
+         }
+              } 
+                   return res;
+               }
--- 12-哈希表/18-四数之和.js
+++ 12-哈希表/18-四数之和.js
@@ -1,5 +1,31 @@
 // 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。
 // 请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] 
 // （若两个四元组元素一一对应，则认为两个四元组重复）：
-
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+ var fourSum = function(nums, target) {
+    const len = nums.length;
+    if(len<4) return [];
+    nums.sort((a,b)=>a-b);
+    const res = [];
+    for(let i = 0;i<len-3;i++){
+        if(i>0&&nums[i]===nums[i-1]) continue;
+        for(let j = i+1;j<len-2;j++){
+            if(j>i+1&&nums[j] ===nums[j-1]) continue;
+            let l=j+1,r=len-1;
+            while(l<r){
+                const sum = nums[i]+nums[j]+nums[l]+nums[r];
+                if(sum>target) {r--;continue;}
+                if(sum<target) {l++;continue;}
+                res.push([nums[i],nums[j],nums[l],nums[r]]);
+                while(l<r&&nums[l]===nums[++l]);
+                while(l<r&&nums[r]===nums[--r]);
+            }
+        }
+    }
+    return res;
+};
  
