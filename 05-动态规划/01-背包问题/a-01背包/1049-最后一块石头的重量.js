e6f1bcb9f56de4bad8e1652930eb1c95acdc8b35
jskhds <2987163443a@gmail.com>
2022-02-24 23:25:04 +0800

    01背包

--- 05-动态规划/01-背包问题/a-01背包/1049-最后一块石头的重量.js
+++ 05-动态规划/01-背包问题/a-01背包/1049-最后一块石头的重量.js
@@ -2,23 +2,21 @@
  * @param {number[]} stones
  * @return {number}
  */
-var lastStoneWeightII = function(stones) {
-    // dp 数组最大容量是 max(stones[length]) * max(stones[i]) /2 + 1
-    // dp[j]的含义是 容量为j的背包 最多可以背dp[j]这么多的石头 
-    let dp =  new Array(15001).fill(0);    
-    let sum = 0;
+  var lastStoneWeightII = function(stones) {
+    // 尽量分成大小相等的两堆，最后的差值就是答案
+    // dp[j] 容量为 j 的背包可以放下的最大重量，不一定能装满，但肯定不能超过 j
+    // dp数组的最大容量应该是 stones 总重量的一半(向下取整)，注意，不一定能装满
+    let sum = eval(stones.join("+"))
+    let targetWeight = parseInt(sum/2)
+    console.log(sum,targetWeight)
+    // 长度应该是 1501，因为有可能最后容量刚好为 1500，需要拿到 dp[1500] 的值
+    let dp = new Array(1501).fill(0);
     for(let i = 0;i<stones.length;i++){
-        sum += stones[i];
+        for(let j = targetWeight;j>=stones[i];j--){
+            dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i])
+        }   
     }
-    // target 需要向下取整 js里的parseInt去掉小数点可以满足
-    let target =parseInt(sum/2);
-    // 遍历 先正序物品 再倒序背包
-    for(let i = 0;i<stones.length;i++){
-        for(let j = target;j>=stones[i];j--){
-            dp[j] =Math.max(dp[j],dp[j - stones[i]] + stones[i])
-            
-        }
-    }
-    return sum - dp[target] - dp[target];
+    // sum - dp[targetWeight] 是剩下的那堆，肯定比 dp[targetWeight] 大，然后再相减即是答案
+    return sum - dp[targetWeight] - dp[targetWeight];
 
 };
